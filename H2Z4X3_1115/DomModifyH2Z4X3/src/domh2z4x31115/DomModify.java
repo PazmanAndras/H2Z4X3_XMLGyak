39977d584727e1148b54aa2ef97d5d0870a18ff3
PAndras <90793913+PazmanAndras@users.noreply.github.com>
2023-11-21 17:49:36 +0100

    8.gyak ✔

--- H2Z4X3_1115/DomModifyH2Z4X3/src/domh2z4x31115/DomModify.java
+++ H2Z4X3_1115/DomModifyH2Z4X3/src/domh2z4x31115/DomModify.java
@@ -28,42 +28,61 @@ public class DomModify {
 		try {
 			File xmlFile = new File("orarendH2Z4X3.xml");
 			File modifiedXmlFile = new File("orarendModifyh2z4x3.xml");
+			
+			
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			
 			DocumentBuilder builder = factory.newDocumentBuilder();
+			
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			
 			Transformer transformer;
+			
+			
 			transformer = transformerFactory.newTransformer();
 			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
 			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			
+			
+			
 			StreamResult console = new StreamResult(System.out);
 			StreamResult modFile = new StreamResult(modifiedXmlFile);
+			
+			
 			
 			
 			//óraren eleem hozááadása
 			Document document = builder.parse(xmlFile);
 			document.getDocumentElement().normalize();
+			
 			System.out.println("a) Óraadó hozzáadása");
 			Element root = document.getDocumentElement();
 			cleanDocument(root);
 			addLecturer(document);
+			
+			
 			DOMSource source = new DOMSource(document);
 			transformer.transform(source, console);
 			transformer.transform(source, modFile);
 			
 			
 			
-			//b
+		
 			System.out.println("\nb) Gyakorlatok előadássá alakítása");
 			changeCourseType(document);
 			source = new DOMSource(document);
 			transformer.transform(source, console);
 			
 			
-			//c
+			
+			
+			
 			System.out.println("\nc) Új óra hozzáadása");
 			addCourse(document);
 			source = new DOMSource(document);
 			transformer.transform(source, console);
+			
+			
 			
 			
 		} catch (IOException | SAXException | ParserConfigurationException | TransformerException e) {
@@ -71,9 +90,15 @@ public class DomModify {
 		}
 	}
 	
+	
+	
 	private static void cleanDocument(Node root) {
+		
+		
 		NodeList nodes = root.getChildNodes();
 		List<Node> toDelete = new ArrayList<>();
+		
+		
 		for(int i=0; i<nodes.getLength(); i++) {
 			if(nodes.item(i).getNodeType()==Node.TEXT_NODE && nodes.item(i).getTextContent().strip().equals("")) {
 				toDelete.add(nodes.item(i));
@@ -81,6 +106,9 @@ public class DomModify {
 				cleanDocument(nodes.item(i));
 			}
 		}
+		
+		
+		
 		for(Node node: toDelete) {
 			root.removeChild(node);
 		}
@@ -89,26 +117,40 @@ public class DomModify {
 	private static void addLecturer(Document document) {
 		Element root = document.getDocumentElement();
 		Element newLecturer = document.createElement("oraado");
-		newLecturer.appendChild(document.createTextNode("Óraadó Anna"));
+		
+		
+		newLecturer.appendChild(document.createTextNode("Nagy Anna"));
 		NodeList courses = root.getElementsByTagName("ora");
 		for(int i=0; i<courses.getLength(); i++) {
 			Node course = courses.item(i);
 			if(((Element)course).getElementsByTagName("oraado").getLength()==0) {
 				course.appendChild(newLecturer);
+				
 				break;
 			}
 		}
+		
+		
 	}
 	
+	
+	
 	private static void changeCourseType(Document document) {
+		
 		Element root = document.getDocumentElement();
+		
 		NodeList courses = root.getElementsByTagName("ora");
+		
 		for(int i=0; i<courses.getLength(); i++) {
 			Node course = courses.item(i);
 			Attr type = (Attr)((Element)course).getAttributeNode("tipus");
 			if(type.getNodeValue().equals("gyakorlat")) type.setNodeValue("előadás");
 		}
+		
 	}
+	
+	
+	
 	private static void addCourse(Document document) {
 		Element root = document.getDocumentElement();
 		
@@ -118,26 +160,34 @@ public class DomModify {
 		newName.appendChild(document.createTextNode("Angol nyelv"));
 		
 		Element newTime = document.createElement("idopont");
+		
 		Element day = document.createElement("nap");
+		
 		day.appendChild(document.createTextNode("péntek"));
+		
 		Element from = document.createElement("tol");
+		
 		from.appendChild(document.createTextNode("8"));
+		
 		Element to = document.createElement("ig");
+		
 		to.appendChild(document.createTextNode("10"));
+		
 		newTime.appendChild(day);
 		newTime.appendChild(from);
+		
 		newTime.appendChild(to);
 		
 		Element newPlace = document.createElement("helyszin");
 		newPlace.appendChild(document.createTextNode("Info 101"));
 		
 		Element newTeacher = document.createElement("oktato");
-		newTeacher.appendChild(document.createTextNode("Alam Károly"));
+		newTeacher.appendChild(document.createTextNode("Kis Janos"));
 		
 		Element newMajor= document.createElement("szak");
-		newMajor.appendChild(document.createTextNode("G3BIW"));
+		newMajor.appendChild(document.createTextNode("GEFIT102"));
 		
-		newCourse.setAttribute("id", "13");
+		newCourse.setAttribute("id", "20");
 		newCourse.setAttribute("tipus", "gyakorlat");
 		newCourse.appendChild(newName);
 		newCourse.appendChild(newTime);
